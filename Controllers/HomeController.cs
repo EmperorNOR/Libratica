aad3c379bc1edaf75ed267de8d20aba06ed8a7f6
Markus Løtveit <68389075+EmperorNOR@users.noreply.github.com>
2020-07-21 17:19:39 +0200

    Small fixes
    Added some content to the Index page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,13 +15,6 @@ namespace Library.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
        
         public ActionResult Contact()
         {
