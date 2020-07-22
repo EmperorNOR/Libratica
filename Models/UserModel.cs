9025e74dba0bf0bd19a0cdc525c24c25f423f4dd
Markus Løtveit <68389075+EmperorNOR@users.noreply.github.com>
2020-07-22 19:41:47 +0200

    Created a user signup form

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -13,14 +13,36 @@ namespace Library.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int user_id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "You need to type in a name")]
+        [Display(Name ="Username")]
         public string user_name { get; set; }
+        [Required(ErrorMessage = "You need to add your age to the site")]
+        [Display(Name = "Age")]
         public int user_age { get; set; }
-        public string user_adress { get; set; }
+        [Required(ErrorMessage = "We require an address")]
+        [Display(Name = "Address")]
+        public string user_address { get; set; }
+        [Required(ErrorMessage = "We require a zipcode")]
+        [Display(Name = "Zipcode")]
         public int user_zipcode { get; set; }
+        [Required(ErrorMessage = "We require an email address")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Mail")]
         public string user_mail { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Compare("user_mail", ErrorMessage = "Your email and confirm email does not match")]
+        [Display(Name = "Confirm Email")]
+        public string user_confirmmail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string user_password { get; set; }
-        public int user_books { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("user_password", ErrorMessage ="Your password and confirm password does not match")]
+        [Display(Name = "Confirm Password")]
+        public string user_confirmpassword { get; set; }
         public virtual IList<Book> Books { get; set; }
     }
 }
