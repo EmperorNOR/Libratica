889d01ad1190af2cb031a94fdef90622c5e08520
Markus Løtveit <68389075+EmperorNOR@users.noreply.github.com>
2020-08-03 07:19:52 +0200

    Basic profile page
    Got a basic profile page working showing the mail you are logged in with.

---
+++ BusinessLogic/ProfileInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Library.Models;
+
+namespace Library.BusinessLogic
+{
+    public class ProfileInfo
+    {
+        public static string ProfileLogic()
+        {
+           string mail = HttpContext.Current.Request.Cookies["user"].Value.ToString();
+            return mail;
+
+
+        }
+    }
+}
