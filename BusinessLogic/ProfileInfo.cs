6f451d470b1a10a656acb307864f94bc471b75ff
Markus Løtveit <68389075+EmperorNOR@users.noreply.github.com>
2020-08-20 10:09:24 +0200

    Return proper data
    Fixed so that the profile view returns proper data, still get a compliation error in regards to linq query for UserHasbooks.(Several elements invalidexception)

--- BusinessLogic/ProfileInfo.cs
+++ BusinessLogic/ProfileInfo.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Library.Models;
+using Library.ViewModels;
 
 namespace Library.BusinessLogic
 {
     public class ProfileInfo
     {
-        public static User ProfileLogic()
+        public static ProfileViewModel ProfileLogic()
         {
-           //string mail = HttpContext.Current.Request.Cookies["user"].Value.ToString();
+           
 
             var email = HttpContext.Current.Request.Cookies["user"].Value.ToString();
 
@@ -19,19 +20,29 @@ namespace Library.BusinessLogic
                 var user = context.Users.SingleOrDefault(u => u.user_mail.ToLower() == email.ToLower());
                 var data = new User
                 {
+                    user_id = user.user_id,
                     user_name = user.user_name,
                     user_age = user.user_age,
                     user_address = user.user_address,
                     user_zipcode = user.user_zipcode,
                     user_mail = user.user_mail,
+                   
 
                 };
-                return (data);
+                var userhasbooks = context.UsersBooks.SingleOrDefault(u => u.users_user_id == user.user_id);
+                var databooks = new UsersHasBooksModel
+                {
+                    users_user_id = userhasbooks.users_user_id,
+                    books_book_id = userhasbooks.books_book_id
+                };
+                
+
+                return (new ProfileViewModel { User = data, book = databooks});
 
             }
-           
-
-
+         
             }
+
+
     }
 }
---
+++ ViewModels/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Library.Models;
+
+namespace Library.BusinessLogic
+{
+    public class ProfileViewModel
+    {
+        public User User { get; set; }
+
+        public UsersHasBooksModel book { get; set; }
+    }
+}
